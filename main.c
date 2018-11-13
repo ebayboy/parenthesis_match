50d7ec1c08bf502233cf5bada80a8d458f51529f
fanpengfei <fanpengfei@jd.com>
2018-11-13 01:36:09 -0500

    fw

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #define STACKINCREAMENT 10
 #define STACK_INIT_SIZE 100
 #define OVERFLOW -2
 #define OK 1
 #define ERROR 0
+
+static int fread_file(const char *filename, unsigned char **buf, int *buflen);
+
 
 typedef int status;
 //typedef char selement_t;
@@ -139,37 +143,68 @@ status Parenthesis_match (sqstack_t * s, char *str)
 
 int main (int argc, char **argv)
 {
-    FILE *fp = NULL;
-    int temp_len = 0;
-
-    char str[BUFSIZ] = {0};
+    unsigned char *buf = NULL;
+    int buflen = 0;
     sqstack_t s;
 
     Init (&s);
 
-#if 1
-    if ((fp = fopen("rule.txt", "r")) == NULL) {
-        fprintf(stderr, "open error!\n");
+    if (fread_file("rule.txt",  &buf,  &buflen) == -1) {
+        fprintf(stderr, "fread_file error!\n");
         return -1;
     }
 
-    if (fread (str, sizeof(str) - 1, 1, fp) != 1) {
-        fprintf(stderr, "read error!\n");
-        return -1;
-    }
+    printf("buf:[%s] len:[%d]\n", buf, buflen);
 
-#else 
-    printf ("请输入字符串:\n");
-    scanf ("%s", str);
-    scanf ("%c", &enter);
-#endif
-
-    Parenthesis_match (&s, str);
+    Parenthesis_match (&s, buf);
 
 out:
-    if (fp) {
-        fclose(fp);
+    if (buf) {
+        free(buf);
     }
 
-    return -1;
+    return 0;
 }
+
+static int fread_file(const char *filename, unsigned char **buf, int *buflen)
+{
+    FILE *fp = NULL;
+    unsigned char *temp = NULL;
+    size_t fsize = 0;
+    fpos_t fpos; 
+
+    if (filename == NULL || buf == NULL  || buflen == NULL) {
+        return -1; 
+    }   
+
+    if ((fp = fopen(filename, "r")) == NULL) {
+        return -1; 
+    }   
+
+    fgetpos(fp, &fpos); 
+    fseek(fp, 0, SEEK_END);
+    if ((fsize = ftell(fp)) == -1) {
+        fclose(fp);
+        return -1; 
+    }   
+    fsetpos(fp,&fpos); 
+
+    if ((temp = (char*)malloc(fsize + 1)) == NULL) {
+        fclose(fp);
+        return -1; 
+    }   
+    memset(temp, 0, sizeof(temp));
+
+    if (fread(temp, fsize, 1, fp) != 1) {
+        fclose(fp);
+        return -1; 
+    }   
+
+    *buf = temp;
+    *buflen = fsize;
+
+    fclose(fp);
+
+    return 0;
+}
+
